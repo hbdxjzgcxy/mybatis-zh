7d5e99b135655e6fdac51e6803afefd6905e1396
hbdxjzgcxy <1301690671@qq.com>
2019-06-14 14:50:25 +0800

    初始化

---
+++ src/main/java/com/xdtech/mybatis/User.java
@@ -0,0 +1,12 @@
+package com.xdtech.mybatis;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+    private String name;
+    private int age;
+    private String password;
+
+}
